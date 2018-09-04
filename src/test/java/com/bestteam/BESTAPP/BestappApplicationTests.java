ec6a1cc3db905fd70ca70acc498a57eb664baf11
Sander <sander.kivivare@gmail.com>
2018-09-04 09:26:36 +0300

    Spring setup

---
+++ src/test/java/com/bestteam/BESTAPP/BestappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bestteam.BESTAPP;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BestappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
