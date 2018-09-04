8f6309405bed27af517de82dd439c7f88d8716d9
Sander <sander.kivivare@gmail.com>
2018-09-04 09:45:30 +0300

    Rename project

--- src/test/java/com/bestteam/BESTAPP/BestappApplicationTests.java
+++ src/test/java/com/bestteam/BESTAPP/BestappApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bestteam.BESTAPP;
+package com.bestteam.CashMoneyMarket;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BestappApplicationTests {
+public class CashMoneyMarketApplicationTests {
 
 	@Test
 	public void contextLoads() {
