9fc9386d7a140a7f5730334ab53e9f690403445c
Sander <sander.kivivare@gmail.com>
2018-12-02 22:57:13 +0200

    for testing purposes

--- src/main/java/com/bestteam/CashMoneyMarket/WebSecurityConfig.java
+++ src/main/java/com/bestteam/CashMoneyMarket/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
@@ -14,12 +13,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/", "/login", "/logout", "/register", "/peek-post", "/images/**", "/css/**", "/js/**").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .oauth2Login()
-                .loginPage("/login")
-                .and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/").permitAll();
+                .anyRequest().permitAll();
     }
 }
