ca3bd6aad1f7e3fac5c1c33f4a0cb2310249d9d6
Sander <sander.kivivare@gmail.com>
2018-12-04 07:56:29 +0200

    Fix security

--- src/main/java/com/bestteam/CashMoneyMarket/WebSecurityConfig.java
+++ src/main/java/com/bestteam/CashMoneyMarket/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
@@ -13,6 +14,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/", "/login", "/logout", "/register", "/peek-post", "/images/**", "/css/**", "/js/**").permitAll()
-                .anyRequest().permitAll();
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login()
+                .loginPage("/login")
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/").permitAll();
     }
 }
